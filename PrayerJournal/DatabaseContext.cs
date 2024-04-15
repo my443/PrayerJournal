503051921fb05b9acb7a5a223af5113b7c0b339f
John van Dijk <fortyfour-three@teksavvy.com>
2024-04-15 06:59:24 -0400

    Started adding DBContext and migrations.

---
+++ PrayerJournal/DatabaseContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrayerJournal
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<PrayerItem> Blogs { get; set; }
+
+        public string DbPath { get; }
+
+        public DatabaseContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "prayer_journal.db");
+            
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
+
+//https://learn.microsoft.com/en-us/ef/core/get-started/overview/first-app?tabs=netcore-cli
---
+++ PrayerJournal/Migrations/20240415105646_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrayerJournal.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Summary = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    IsHistory = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blogs");
+        }
+    }
+}
